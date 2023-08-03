d92e9490dea58887d520d9b29eb11daf2dd4ee6c
Sumaiya Khan <sumaiya.khan32665@gmail.com>
2023-08-03 12:35:37 +0600

    added varible answer (5-8)

--- variable.js
+++ variable.js
@@ -1,11 +1,26 @@
-// What is Js?
+//1.What is Js?
 //Ans: Js is a scripted language for web that makes a website interactive.It can change and update HTML abd CSS.It makes the website dynamic.
-//How does the js work?
+//2.How does the js work?
 //Ans:JavaScript is an interpreted language. This means we do not have to compile the JavaScript source code before sending it to the browser. An interpreter can take the raw JavaScript code and run it for you.
-//What is a variable?
+//3.What is a variable?
 //Ans: In JavaScript, a variable stores data that can be changed later on.All JavaScript variables must be identified with unique names.example:var name='Sumaiya'. here name is the identifier that stores data sumaiya that can be changed any time.
-//Declare a Variable
-//Ans:A variable stores data.Ex:var number=20; here number is the identifier 
+//4.Declare a Variable
+//Ans:A variable stores data.Ex:var number=20; here number is the identifier .
+//5.Types of a variable?How can find out the type of the variable?
+//Ans:1.string 2.number 3.boolean
+//var name='sumaiya' is a string type variable
+//var number=20 is a number type variable
+//var isYourNameSumaiya=true is a boolean type variable
+//6.Primitive and non primitive data type
+//Ans:Primitive data types:number,string,boolean,undefined,null
+//non primitive data types:object and array
+//7.Convention of js variables?
+//Ans:Names can contain letters, digits, underscores, and dollar signs.
+//Names must begin with a letter. ex:var name is alright but var 1name is not allowed 
+//Names can also begin with $ and _ (but we will not use it in this tutorial).
+//Names are case sensitive (y and Y are different variables).ex:nameSum and namesum they are not same 
+//Reserved words (like JavaScript keywords) cannot be used as names. ex:var if =90 is not allowed if is a reserved word
+
 
 
 
