f211b5c49adc5fcd552bf6269cb358f7aa305b4a
Sumaiya Khan <sumaiya.khan32665@gmail.com>
2023-08-03 14:16:43 +0600

    added loop answer(1-4)

---
+++ loop.js
@@ -0,0 +1,27 @@
+//1.Display "ajk amr mon valo nei " for 39 times
+
+for(i=1;i<=39;i++){
+    console.log(i+ " "+"Ajk amr mon valo nei");
+}
+
+//2.Display  numbers between 58 to 98
+console.log('Display  numbers between 58 to 98');
+var initialNumber=58;
+while(initialNumber<=98){
+    console.log(initialNumber);
+    initialNumber++;
+ }
+
+ //3.show all even numbers from 412 to 456
+ console.log('All even numbers from 412 to 456');
+ for(var i=412;i<=456;i+=2){
+    console.log(i);
+ }
+
+ //4.Show all odd numbers from 581 to 623
+ console.log('Show all odd numbers from 581 to 623');
+ for(var i=581;i<=623;i+=2){
+    console.log(i);
+ }
+
+
