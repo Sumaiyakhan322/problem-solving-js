0b72e19daaef395cb7ee5074acd4770da9a2d1ff
Sumaiya Khan <sumaiya.khan32665@gmail.com>
2023-08-03 13:11:34 +0600

    added array answer (1-6)

---
+++ array.js
@@ -0,0 +1,27 @@
+//1. what is the purpose of an array?
+//Ans:Array is a collection of same data types.It stores multiple values in a single variable.
+
+//2.declare a array in js
+//Ans:var numbers=[10,20,30,40]
+//var names=["X" , "Y" ,"Z"]
+// we must ensure values that are stored in array same data type
+
+//3.number of elements in an array
+var numbers=[10,20,30,40,50,60]
+console.log(numbers);
+
+
+//4.What is the index of a array?
+//Ans:Index indicates the position of an element of an array.In array index starts with 0
+
+
+//5.Find the value of an element by index?
+numbers=[10,20,30,40,50,60];
+var numberInPosition1=numbers[1];
+console.log(numberInPosition1);
+
+
+//6.Change the element by index
+numbers=[10,20,30,40,50,60];
+numbers[1]=100;
+console.log(numbers);
