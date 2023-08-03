fd47982984fc11a07be66bcb353922043b329af3
Sumaiya Khan <sumaiya.khan32665@gmail.com>
2023-08-03 12:21:58 +0600

    added variable answers

---
+++ variable.js
@@ -0,0 +1,11 @@
+// What is Js?
+//Ans: Js is a scripted language for web that makes a website interactive.It can change and update HTML abd CSS.It makes the website dynamic.
+//How does the js work?
+//Ans:JavaScript is an interpreted language. This means we do not have to compile the JavaScript source code before sending it to the browser. An interpreter can take the raw JavaScript code and run it for you.
+//What is a variable?
+//Ans: In JavaScript, a variable stores data that can be changed later on.All JavaScript variables must be identified with unique names.example:var name='Sumaiya'. here name is the identifier that stores data sumaiya that can be changed any time.
+//Declare a Variable
+//Ans:A variable stores data.Ex:var number=20; here number is the identifier 
+
+
+
