4ba17853c7c3abe0b7b3125d054216d9910dde27
Sumaiya Khan <sumaiya.khan32665@gmail.com>
2023-08-03 13:56:55 +0600

    added array answer(7-12)

--- array.js
+++ array.js
@@ -25,3 +25,47 @@ console.log(numberInPosition1);
 numbers=[10,20,30,40,50,60];
 numbers[1]=100;
 console.log(numbers);
+
+
+//7.Get the index by the value of an elements
+var letters=["A" ,"B" , "C" ,"D" ,"E"];
+console.log(letters);
+var indexOfA=letters.indexOf("A");
+console.log(indexOfA);
+
+
+//8.what does it mean when you get undefined  while getting the value of an element?
+//Ans:when we search  for any element with index but the index  does not belong to the array ,we get undefined. We can find any element by index but when the index  does not belong to the array we get undefined
+var letters=["A" ,"B" , "C" ,"D" ,"E"];
+var findLetterG=letters[6];
+console.log(findLetterG);
+
+
+//9.add element at the last position
+var letters=["A" ,"B" , "C" ,"D" ,"E"];
+console.log(letters);
+var addLetter=letters.push("F");
+console.log(letters);
+
+//10.delete element from the last position
+var letters=["A" ,"B" , "C" ,"D" ,"E"];
+console.log(letters);
+var deleteLastLetter=letters.pop();
+console.log(letters);
+console.log(deleteLastLetter);
+
+
+//11.Add element at the 1st position
+var intNumbers=[1,2,3,4,5];
+console.log(intNumbers);
+var addNewInt=intNumbers.unshift(0);
+console.log(intNumbers);
+
+
+//12.delete element from the 1st position
+var intNumbers=[1,2,3,4,5];
+console.log(intNumbers);
+intNumbers.shift()
+console.log(intNumbers);
+
+
